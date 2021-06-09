48cac9897e45e8c5e8a651cc06ddce3bf1b8465a
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-06-09 23:30:36 +0300

    copt

---
+++ EPPP/Classes/Misc/GlobalDefinitions.swift
@@ -0,0 +1,25 @@
+//
+//  GlobalDefinitions.swift
+//  Nursing
+//
+//  Created by Andrey Chernyshev on 16.01.2021.
+//
+
+final class GlobalDefinitions {
+    static let domainUrl = "https://exams.korrekted.com"
+    static let apiKey = "jpn12b2dGIMuuH0Q"
+
+    static let sdkDomainUrl = "https://analytics.korrekted.com"
+    static let sdkApiKey = "g2Hyav5TNsQ4nNm1"
+
+    static let applicationTag = "EXAM_EPPP"
+
+    static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
+    static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+
+    static let appleAppID = "1569405565"
+
+    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
+    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
+    static let contactUsUrl = "https://quantresear.ch/contact.html"
+}
