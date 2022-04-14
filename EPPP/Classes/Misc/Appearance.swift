a5ce62f647d4e57df192ec0cbd52a06e31bea213
akonst17 <akonst17@gmail.com>
2022-04-14 12:35:22 +0300

    upgrade; build 7

--- EPPP/Classes/Misc/Appearance.swift
+++ EPPP/Classes/Misc/Appearance.swift
@@ -18,4 +18,8 @@ struct Appearance {
     static let progress1Color = UIColor(integralRed: 0, green: 40, blue: 105)
     static let progress2Color = UIColor(integralRed: 237, green: 113, blue: 93)
     static let progress3Color = UIColor(integralRed: 1, green: 96, blue: 153)
+    
+    static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
+    static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
+    static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
 }
--- EPPP/Classes/Service/Stats/Share/StatsShareManager.swift
+++ EPPP/Classes/Service/Stats/Share/StatsShareManager.swift
@@ -16,7 +16,7 @@ final class StatsShareManager {
 // MARK: Public
 extension StatsShareManager {
     func sharedContainerURL() -> URL? {
-        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.eppp.widgets")
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.eppp.widget")
     }
     
     @discardableResult
